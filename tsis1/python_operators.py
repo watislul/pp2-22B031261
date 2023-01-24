38872c24f2aaadc7715d50be4f462392797ef71b
watislul <123138150+watislul@users.noreply.github.com>
2023-01-24 23:49:56 +0600

    Add files via upload

---
+++ tsis1/python_operators.py
@@ -0,0 +1,23 @@
+print(10 + 5)
+
+'''
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+                     Example
++	Addition	     x + y	
+-	Subtraction	     x - y	
+*	Multiplication	 x * y	
+/	Division	     x / y	
+%	Modulus	         x % y	
+**	Exponentiation	 x ** y	
+//	Floor division	 x // y
+'''
+
