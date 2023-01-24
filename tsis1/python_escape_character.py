57f0df58a7c6ba287565b4f5a6b28c413b64dc0f
watislul <123138150+watislul@users.noreply.github.com>
2023-01-24 23:36:30 +0600

    Add files via upload

---
+++ tsis1/python_escape_character.py
@@ -0,0 +1,17 @@
+'''
+txt = "We are the so-called "Vikings" from the north."
+'''
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+
+'''
+\'	Single quote
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed
+\ooo    Octal value	
+'''''
