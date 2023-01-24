965587bad0b89a3cc8717f9b00c67056d85fee8e
watislul <123138150+watislul@users.noreply.github.com>
2023-01-24 23:29:16 +0600

    Add files via upload

---
+++ tsis1/python_modify_strings.py
@@ -0,0 +1,14 @@
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
