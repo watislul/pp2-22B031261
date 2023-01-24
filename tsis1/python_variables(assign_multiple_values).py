02a259e353880fc56a88bb71fe0cf81b38400d39
watislul <123138150+watislul@users.noreply.github.com>
2023-01-24 22:58:05 +0600

    Add files via upload

---
+++ tsis1/python_variables(assign_multiple_values).py
@@ -0,0 +1,17 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#Note: Make sure the number of variables matches the number of values, or else you will get an error.
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
