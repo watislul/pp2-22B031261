8fcb790e20e725ac4a2e5ee4ec5eca77a4eee271
watislul <123138150+watislul@users.noreply.github.com>
2023-01-24 22:56:07 +0600

    Add files via upload

---
+++ tsis1/python_variables(names).py
@@ -0,0 +1,25 @@
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+print(myvar, my_var, _my_var, myVar, MYVAR, myvar2)
+
+"""""
+2myvar = "John"
+my-var = "John"
+my var = "John"
+that is not correct
+!Remember that variable names are case-sensitive
+"""""
+
+myVariableName = "John"
+print(myVariableName)
+
+MyVariableName = "John"
+print(MyVariableName)
+
+my_variable_name = "John"
+print(my_variable_name)
