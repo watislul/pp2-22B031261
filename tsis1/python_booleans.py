57708420367a367bb41bb5b9177e46e5873f1c50
watislul <123138150+watislul@users.noreply.github.com>
2023-01-24 23:46:51 +0600

    Add files via upload

---
+++ tsis1/python_booleans.py
@@ -0,0 +1,63 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+print('\n')
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+print('\n')
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+print('\n')
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+print('\n')
+
+x = 200
+print(isinstance(x, int))
