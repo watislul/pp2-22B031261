29c7bfaa447f15794d1f19d5430a5c341fd0f5ff
watislul <123138150+watislul@users.noreply.github.com>
2023-01-24 23:09:04 +0600

    Add files via upload

---
+++ tsis1/python_data_types.py
@@ -0,0 +1,14 @@
+"""""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""""
+
+x = 5
+print(type(x)) # You can get the data type of any object by using the type() function:
+
