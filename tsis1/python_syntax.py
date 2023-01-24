8d31b864447c1d5268ace165b372689f1cfbe017
watislul <123138150+watislul@users.noreply.github.com>
2023-01-24 22:18:17 +0600

    Add files via upload

---
+++ tsis1/python_syntax.py
@@ -0,0 +1,18 @@
+if 5 > 2:
+    print("Five is greater than two!")
+"""
+if 5 > 2:
+print("Five is greater than two!")
+that is not correct
+"""
+
+if 5 > 2:
+    print("Five is greater than two!")
+if 5 > 2:
+    print("Five is greater than two!")
+""""" 
+if 5 > 2:
+print("Five is greater than two")
+    print("Five is greater than two")
+that too is not correct
+"""""
