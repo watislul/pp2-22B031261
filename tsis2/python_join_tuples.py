0617bb1c5a7f2723122cfa6651b969359d86b309
watislul <123138150+watislul@users.noreply.github.com>
2023-02-01 23:52:59 +0600

    Add files via upload

--- tsis2/python_join_tuples.py
+++ tsis2/python_join_tuples.py
@@ -9,3 +9,12 @@ mytuple = fruits * 2
 
 print(mytuple)
 
+
+# Python Methods
+
+'''
+Method	        Description
+count()	        Returns the number of times a specified value occurs in a tuple
+index()	        Searches the tuple for a specified value and returns the position of where it was found
+
+'''
