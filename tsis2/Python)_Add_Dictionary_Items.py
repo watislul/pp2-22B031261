e272a4f878620e22994d813f02f2705835f7982b
watislul <123138150+watislul@users.noreply.github.com>
2023-02-02 00:19:37 +0600

    Add files via upload

---
+++ tsis2/Python)_Add_Dictionary_Items.py
@@ -0,0 +1,17 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+
+print(thisdict)
+
