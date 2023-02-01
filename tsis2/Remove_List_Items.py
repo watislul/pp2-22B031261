d5a89378385c22abd2b29ffc819adc5a2b9cf750
watislul <123138150+watislul@users.noreply.github.com>
2023-02-01 22:12:01 +0600

    Add files via upload

---
+++ tsis2/Remove_List_Items.py
@@ -0,0 +1,22 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
