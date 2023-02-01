e3b0d384f357a5b98af5bd5f283dab4c646058ed
watislul <123138150+watislul@users.noreply.github.com>
2023-02-01 23:57:53 +0600

    Add files via upload

---
+++ tsis2/python_sets.py
@@ -0,0 +1,40 @@
+myset = {"apple", "banana", "cherry"}
+
+print(myset)
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set1)
+print(set2)
+print(set3)
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+
+'''
+Python Collections (Arrays)
+There are four collection data types in the Python programming language:
+
+1)  List is a collection which is ordered and changeable. Allows duplicate members.
+2)  Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+3)  Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+4)  Dictionary is a collection which is ordered** and changeable. No duplicate members.
+'''
