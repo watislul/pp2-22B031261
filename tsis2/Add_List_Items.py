9586b46743e8c47d50b9e5ba0d888b684ebf3327
watislul <123138150+watislul@users.noreply.github.com>
2023-02-01 22:10:13 +0600

    Add files via upload

---
+++ tsis2/Add_List_Items.py
@@ -0,0 +1,17 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
