64c8e4bf3ed9a55cfe0ce8d17bc94621bb5e5f56
watislul <123138150+watislul@users.noreply.github.com>
2023-02-01 23:49:09 +0600

    Add files via upload

---
+++ tsis2/python_unpack_tuples.py
@@ -0,0 +1,26 @@
+fruits = ("apple", "banana", "cherry")
+print(fruits)
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
