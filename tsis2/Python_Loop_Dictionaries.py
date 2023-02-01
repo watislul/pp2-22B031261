74164206aa072281e89f95e4ca124de18239c60f
watislul <123138150+watislul@users.noreply.github.com>
2023-02-02 00:23:43 +0600

    Add files via upload

---
+++ tsis2/Python_Loop_Dictionaries.py
@@ -0,0 +1,21 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+
+for x in thisdict:
+  print(x)
+
+for x in thisdict:
+  print(thisdict[x])
+
+for x in thisdict.values():
+  print(x)
+
+for x in thisdict.keys():
+  print(x)
+
+for x, y in thisdict.items():
+  print(x, y)
