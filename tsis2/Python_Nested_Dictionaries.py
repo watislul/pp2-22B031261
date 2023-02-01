c996dacdf0086ec31790a182d336343805e10001
watislul <123138150+watislul@users.noreply.github.com>
2023-02-02 00:26:27 +0600

    Add files via upload

---
+++ tsis2/Python_Nested_Dictionaries.py
@@ -0,0 +1,38 @@
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily)
+
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+print(child1)
+print(child2)
+print(child3)
+print(myfamily)
